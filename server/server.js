db1a7d3ae6e21e6b948f21c628dfaa393b3acc9e
DiegoCass01 <diego.castillo170901@gmail.com>
2025-03-20 01:26:58 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const users = [
+  { id: "1", name: "John Doe", age: 30, isMarried: true },
+  { id: "2", name: "Jane Smith", age: 25, isMarried: false },
+  { id: "3", name: "Alice Johnson", age: 28, isMarried: false },
+];
+
+const typeDefs = `
+  type Query {
+    getUsers: [User]
+    getUserById(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(name: String!, age: Int!, isMarried: Boolean!): User 
+  }
+
+  type User {
+    id: ID
+    name: String
+    age: Int
+    isMarried: Boolean
+  }
+`;
+
+const resolvers = {};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server Running at: ${url}`);
